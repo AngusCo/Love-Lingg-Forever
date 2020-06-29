951c0a75aa0fe21c681f05f2d860ce46e5aa0e1a
AngusKo <anan20.tw@gmail.com>
2020-06-29 15:26:04 +0800

    52 期上線

---
+++ tracks/no52/tracks.js
@@ -0,0 +1,79 @@
+var	tracks = [{
+		"track": 1,
+		"topic": "2020年7月號",
+		"name": "小行星幼兒誌",
+		"duration": "03:06",
+		"file": "01.mp3"
+	},{
+		"track": 2,
+		"topic": "情緒劇場",
+		"name": "樂樂輸球了",
+		"duration": "05:07",
+		"file": "02.mp3"
+	},{
+		"track": 3,
+		"topic": "讀首小詩",
+		"name": "比一比",
+		"duration": "02:18",
+		"file": "03.mp3"
+	},{
+		"track": 4,
+		"topic": "腦筋轉轉彎",
+		"name": "我快輸了，怎麼辦？",
+		"duration": "03:51",
+		"file": "04.mp3"
+	},{
+		"track": 5,
+		"topic": "知識不思議",
+		"name": "來，我們來比賽！",
+		"duration": "10:48",
+		"file": "05.mp3"
+	},{
+		"track": 6,
+		"topic": "小小探索家",
+		"name": "皮球為什麼彈高高",
+		"duration": "05:22",
+		"file": "06.mp3"
+	},{
+		"track": 7,
+		"topic": "幼年童話",
+		"name": "潮間帶的音樂會",
+		"duration": "05:59",
+		"file": "07.mp3"
+	},{
+		"track": 8,
+		"topic": "動物新鮮事",
+		"name": "蝗蟲來了！",
+		"duration": "03:58",
+		"file": "08.mp3"
+	},{
+		"track": 9,
+		"topic": "身體怎麼了",
+		"name": "口角炎為什麼會發作？",
+		"duration": "02:49",
+		"file": "09.mp3"
+	},{
+		"track": 10,
+		"topic": "故事音樂盒",
+		"name": "小木偶",
+		"duration": "08:36",
+		"file": "10.mp3"
+	},{
+		"track": 11,
+		"topic": "故事摩天輪",
+		"name": "調皮小猴子愛玩水 2",
+		"duration": "05:12",
+		"file": "11.mp3"
+	},{
+		"track": 12,
+		"topic": "小行星點讀 ABC",
+		"name": "Five Green and Speckied Frogs",
+		"duration": "19:26",
+		"file": "12.mp3"
+	},{
+		"track": 13,
+		"topic": "2020年7月號",
+		"name": "小行星的祝福",
+		"duration": "02:52",
+		"file": "13.mp3"
+	}];
