2213f61eba65244baa1f831ee37e622ee6b2728a
AngusKo <anan20.tw@gmail.com>
2020-12-01 14:55:59 +0800

    57 期上架

---
+++ medias/小行星57期-舊衣服，變魔術/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+  {
+    "album": "57期",
+    "title": "2020年12月號小行星幼兒誌",
+    "track": "01",
+    "duration": "3:05",
+    "file": "01-2020年12月號小行星幼兒誌.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "情緒劇場 樂樂玩偶，變身！",
+    "track": "02",
+    "duration": "5:06",
+    "file": "02-情緒劇場 樂樂玩偶，變身！.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "動物新鮮事 小蛇換新衣",
+    "track": "03",
+    "duration": "3:25",
+    "file": "03-動物新鮮事 小蛇換新衣.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "知識不思議 舊衣服，變魔術！",
+    "track": "04",
+    "duration": "10:17",
+    "file": "04-知識不思議 舊衣服，變魔術！.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "小小探索家 寶特瓶，變衣服？",
+    "track": "05",
+    "duration": "3:53",
+    "file": "05-小小探索家 寶特瓶，變衣服？.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "讀首小詩 小小的雪花",
+    "track": "06",
+    "duration": "1:59",
+    "file": "06-讀首小詩 小小的雪花.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "幼年童話 調皮的小精靈",
+    "track": "07",
+    "duration": "5:34",
+    "file": "07-幼年童話 調皮的小精靈.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "身體怎麼了 我的頭髮缺一塊？",
+    "track": "08",
+    "duration": "4:03",
+    "file": "08-身體怎麼了 我的頭髮缺一塊？.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "STEAM這樣玩 好吃又驚奇的蛋",
+    "track": "09",
+    "duration": "4:00",
+    "file": "09-STEAM這樣玩 好吃又驚奇的蛋.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "世界大不同 奧地利音樂耶誕夜",
+    "track": 10,
+    "duration": "5:18",
+    "file": "10-世界大不同 奧地利音樂耶誕夜.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "故事摩天輪 時間錯亂瓜1",
+    "track": 11,
+    "duration": "5:38",
+    "file": "11-故事摩天輪 時間錯亂瓜1.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "小行星點讀ABC Pussy Cat Pussy Cat",
+    "track": 12,
+    "duration": "11:14",
+    "file": "12-小行星點讀ABC Pussy Cat Pussy Cat.mp3"
+  },
+  {
+    "album": "57期",
+    "title": "2020年12月號小行星的祝福",
+    "track": 13,
+    "duration": "2:59",
+    "file": "13-2020年12月號小行星的祝福.mp3"
+  }
+];
