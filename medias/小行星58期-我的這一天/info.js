908b499905801213b1e56f9e247bb916e29de589
AngusKo <anan20.tw@gmail.com>
2021-01-26 11:53:43 +0800

    58.59 期上架

---
+++ medias/小行星58期-我的這一天/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "58期",
+        "title": "2021年1月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01-2021年1月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "情緒劇場 康康慢吞吞",
+        "track": "02",
+        "duration": "5:49",
+        "file": "02-情緒劇場 康康慢吞吞.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "讀首小詩 算時間",
+        "track": "03",
+        "duration": "2:16",
+        "file": "03-讀首小詩 算時間.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "腦筋轉轉彎 我還想繼續玩",
+        "track": "04",
+        "duration": "3:32",
+        "file": "04-腦筋轉轉彎 我還想繼續玩.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "知識不思議 小怪獸的一天",
+        "track": "05",
+        "duration": "12:13",
+        "file": "05-知識不思議 小怪獸的一天.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "小小探索家 為什麼白天變成晚上了？",
+        "track": "06",
+        "duration": "5:57",
+        "file": "06-小小探索家 為什麼白天變成晚上了？.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "幼年童話 好朋友的家",
+        "track": "07",
+        "duration": "5:58",
+        "file": "07-幼年童話 好朋友的家.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "動物新鮮事 無尾熊愛睡覺",
+        "track": "08",
+        "duration": "3:58",
+        "file": "08-動物新鮮事 無尾熊愛睡覺.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "身體怎麼了 舌頭燙傷了",
+        "track": "09",
+        "duration": "3:20",
+        "file": "09-身體怎麼了 舌頭燙傷了.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "故事音樂盒 灰姑娘",
+        "track": 10,
+        "duration": "8:22",
+        "file": "10-故事音樂盒 灰姑娘.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "故事摩天輪 時間錯亂瓜2",
+        "track": 11,
+        "duration": "5:14",
+        "file": "11-故事摩天輪 時間錯亂瓜2.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "小行星點讀ABC Are You Sleeping?",
+        "track": 12,
+        "duration": "12:47",
+        "file": "12-小行星點讀ABC Are You Sleeping?.mp3"
+    },
+    {
+        "album": "58期",
+        "title": "2021年1月號小行星的祝福",
+        "track": 13,
+        "duration": "2:59",
+        "file": "13-2021年1月號小行星的祝福.mp3"
+    }
+];
---
+++ medias/小行星59期-好吃的食物/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "59期",
+        "title": "2021年2月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01-2021年2月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "情緒劇場 樂樂吃青花菜",
+        "track": "02",
+        "duration": "5:37",
+        "file": "02-情緒劇場 樂樂吃青花菜.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "讀首小詩 吃東西",
+        "track": "03",
+        "duration": "2:12",
+        "file": "03-讀首小詩 吃東西.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "動物新鮮事 切葉蟻種蘑菇",
+        "track": "04",
+        "duration": "3:31",
+        "file": "04-動物新鮮事 切葉蟻種蘑菇.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "知識不思議 今晚吃什麼？",
+        "track": "05",
+        "duration": "11:36",
+        "file": "05-知識不思議 今晚吃什麼？.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "幼年童話 超級美味草",
+        "track": "06",
+        "duration": "5:38",
+        "file": "06-幼年童話 超級美味草.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "生活大發現 潔牙小幫手 牙刷",
+        "track": "07",
+        "duration": "4:51",
+        "file": "07-生活大發現 潔牙小幫手 牙刷.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "身體怎麼了 過年不熬夜",
+        "track": "08",
+        "duration": "4:03",
+        "file": "08-身體怎麼了 過年不熬夜.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "小小探索家 為什麼麵包烤焦會變黑？",
+        "track": "09",
+        "duration": "5:25",
+        "file": "09-小小探索家 為什麼麵包烤焦會變黑？.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "臺灣好好玩 到南投竹山彩繪燈籠",
+        "track": 10,
+        "duration": "4:09",
+        "file": "10-臺灣好好玩 到南投竹山彩繪燈籠.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "故事摩天輪 時間錯亂瓜3",
+        "track": 11,
+        "duration": "5:42",
+        "file": "11-故事摩天輪 時間錯亂瓜3.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "小行星點讀ABC I've Been Working on the Railroad",
+        "track": 12,
+        "duration": "12:43",
+        "file": "12-小行星點讀ABC I've Been Working on the Railroad.mp3"
+    },
+    {
+        "album": "59期",
+        "title": "2021年2月號小行星的祝福",
+        "track": 13,
+        "duration": "2:55",
+        "file": "13-2021年2月號小行星的祝福.mp3"
+    }
+];
