7a388a24004b29253b72a9a0fef609ad16411e1e
AngusKo <anan20.tw@gmail.com>
2021-03-08 10:54:50 +0800

    60 期上架

---
+++ medias/小行星60期-照顧我們的地球/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "60期",
+        "title": "2021年3月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01-2021年3月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "情緒劇場 留給寄居蟹乾淨的家",
+        "track": "02",
+        "duration": "5:20",
+        "file": "02-情緒劇場 留給寄居蟹乾淨的家.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "讀首小詩 沙灘上的小屋子",
+        "track": "03",
+        "duration": "2:09",
+        "file": "03-讀首小詩 沙灘上的小屋子.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "腦筋轉轉彎 一定要帶環保餐具嗎？",
+        "track": "04",
+        "duration": "3:54",
+        "file": "04-腦筋轉轉彎 一定要帶環保餐具嗎？.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "知識不思議 做地球的好朋友",
+        "track": "05",
+        "duration": "8:48",
+        "file": "05-知識不思議 做地球的好朋友.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "幼年童話 一二三，木頭人",
+        "track": "06",
+        "duration": "6:46",
+        "file": "06-幼年童話 一二三，木頭人.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "動物新鮮事 蚯蚓鑽啊鑽",
+        "track": "07",
+        "duration": "3:19",
+        "file": "07-動物新鮮事 蚯蚓鑽啊鑽.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "身體怎麼了 手被門夾到了",
+        "track": "08",
+        "duration": "3:32",
+        "file": "08-身體怎麼了 手被門夾到了.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "小小探索家 海上為什麼有電風扇？",
+        "track": "09",
+        "duration": "5:00",
+        "file": "09-小小探索家 海上為什麼有電風扇？.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "故事音樂盒 龜兔賽跑",
+        "track": 10,
+        "duration": "8:30",
+        "file": "10-故事音樂盒 龜兔賽跑.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "故事摩天輪 小剪刀魚1",
+        "track": 11,
+        "duration": "5:12",
+        "file": "11-故事摩天輪 小剪刀魚1.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "小行星點讀ABC Baby Bumblebee",
+        "track": 12,
+        "duration": "10:15",
+        "file": "12-小行星點讀ABC Baby Bumblebee.mp3"
+    },
+    {
+        "album": "60期",
+        "title": "2021年3月號小行星的祝福",
+        "track": 13,
+        "duration": "2:59",
+        "file": "13-2021年3月號小行星的祝福.mp3"
+    }
+];
