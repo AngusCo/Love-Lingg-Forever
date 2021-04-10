cf257dc6aefe424ae577fa38c36c1022e3f212c6
AngusKo <anan20.tw@gmail.com>
2021-04-10 11:17:27 +0800

    61 期上架

---
+++ medias/小行星61期-對抗病毒大作戰/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "61期",
+        "title": "2021年4月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01-2021年4月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "情緒劇場 校外教學取消了",
+        "track": "02",
+        "duration": "6:27",
+        "file": "02-情緒劇場 校外教學取消了.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "動物新鮮事 浣熊愛乾淨？",
+        "track": "03",
+        "duration": "3:56",
+        "file": "03-動物新鮮事 浣熊愛乾淨？.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "知識不思議 傳染病，不要來！",
+        "track": "04",
+        "duration": "13:01",
+        "file": "04-知識不思議 傳染病，不要來！.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "小小探索家 酒精為什麼能消毒？",
+        "track": "05",
+        "duration": "5:05",
+        "file": "05-小小探索家 酒精為什麼能消毒？.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "讀首小詩 喜歡春天",
+        "track": "06",
+        "duration": "2:20",
+        "file": "06-讀首小詩 喜歡春天.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "幼年童話 雪精靈",
+        "track": "07",
+        "duration": "5:08",
+        "file": "07-幼年童話 雪精靈.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "STEAM這樣玩 千變萬化的果汁",
+        "track": "08",
+        "duration": "6:13",
+        "file": "08-STEAM這樣玩 千變萬化的果汁.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "身體怎麼了 小心牽拉肘",
+        "track": "09",
+        "duration": "3:15",
+        "file": "09-身體怎麼了 小心牽拉肘.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "世界大不同 走進丹麥童話世界",
+        "track": 10,
+        "duration": "5:52",
+        "file": "10-世界大不同 走進丹麥童話世界.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "故事摩天輪 小剪刀魚2",
+        "track": 11,
+        "duration": "5:20",
+        "file": "11-故事摩天輪 小剪刀魚2.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "小行星點讀ABC Baby Old MacDonald",
+        "track": 12,
+        "duration": "14:37",
+        "file": "12-小行星點讀ABC Baby Old MacDonald.mp3"
+    },
+    {
+        "album": "61期",
+        "title": "2021年4月號小行星的祝福",
+        "track": 13,
+        "duration": "2:59",
+        "file": "13-2021年4月號小行星的祝福.mp3"
+    }
+];
