116690ca93496ee5358a2c6f89be2a89417185ed
AngusKo <anan20.tw@gmail.com>
2021-04-29 17:08:09 +0800

    62 期上架

---
+++ medias/小行星62期-我從哪裡來？/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "62期",
+        "title": "2021年5月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01-2021年5月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "情緒劇場 康康的大肚子",
+        "track": "02",
+        "duration": "5:12",
+        "file": "02-情緒劇場 康康的大肚子.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "讀首小詩 我從哪裡來？",
+        "track": "03",
+        "duration": "2:19",
+        "file": "03-讀首小詩 我從哪裡來？.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "腦筋轉轉彎 生日一定要慶生嗎？",
+        "track": "04",
+        "duration": "3:48",
+        "file": "04-腦筋轉轉彎 生日一定要慶生嗎？.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "知識不思議 我是怎麼來的呢？",
+        "track": "05",
+        "duration": "8:44",
+        "file": "05-知識不思議 我是怎麼來的呢？.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "小小探索家 為什麼超音波可以照出小寶寶？",
+        "track": "06",
+        "duration": "4:36",
+        "file": "06-小小探索家 為什麼超音波可以照出小寶寶？.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "幼年童話 工蟻小妹的鈕扣山",
+        "track": "07",
+        "duration": "5:38",
+        "file": "07-幼年童話 工蟻小妹的鈕扣山.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "動物新鮮事 鴨嘴獸不是鴨",
+        "track": "08",
+        "duration": "3:59",
+        "file": "08-動物新鮮事 鴨嘴獸不是鴨.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "身體怎麼了 我放屁了！",
+        "track": "09",
+        "duration": "3:55",
+        "file": "09-身體怎麼了 我放屁了！.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "故事音樂盒 小飛俠",
+        "track": 10,
+        "duration": "8:54",
+        "file": "10-故事音樂盒 小飛俠.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "故事摩天輪 小剪刀魚3",
+        "track": 11,
+        "duration": "4:59",
+        "file": "11-故事摩天輪 小剪刀魚3.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "小行星點讀ABC Baby An Old Womam Who Lived in a Shoe",
+        "track": 12,
+        "duration": "10:15",
+        "file": "12-小行星點讀ABC Baby An Old Womam Who Lived in a Shoe.mp3"
+    },
+    {
+        "album": "62期",
+        "title": "2021年5月號小行星的祝福",
+        "track": 13,
+        "duration": "3:04",
+        "file": "13-2021年5月號小行星的祝福.mp3"
+    }
+];
