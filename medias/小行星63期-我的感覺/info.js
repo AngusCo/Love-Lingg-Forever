d8057384c9ec06a23291835b0b644d6dff034932
AngusKo <anan20.tw@gmail.com>
2021-07-07 10:05:54 +0800

    63.64 期上架

---
+++ medias/小行星63期-我的感覺/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "63期",
+        "title": "2021年6月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01-2021年6月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "情緒劇場 心心的手鍊",
+        "track": "02",
+        "duration": "6:26",
+        "file": "02-情緒劇場 心心的手鍊.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "讀首小詩 花的臉",
+        "track": "03",
+        "duration": "2:22",
+        "file": "03-讀首小詩 花的臉.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "腦筋轉轉彎 別人的新玩具",
+        "track": "04",
+        "duration": "3:56",
+        "file": "04-腦筋轉轉彎 別人的新玩具.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "知識不思議 我的感覺",
+        "track": "05",
+        "duration": "10:25",
+        "file": "05-知識不思議 我的感覺.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "動物新鮮事 小狗的心情",
+        "track": "06",
+        "duration": "4:03",
+        "file": "06-動物新鮮事 小狗的心情.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "幼年童話 小花豹，抱抱",
+        "track": "07",
+        "duration": "4:58",
+        "file": "07-幼年童話 小花豹，抱抱.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "身體怎麼了 我的眼睛眨不停",
+        "track": "08",
+        "duration": "3:24",
+        "file": "08-身體怎麼了 我的眼睛眨不停.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "小小探索家 測謊機為什麼知道我說謊？",
+        "track": "09",
+        "duration": "4:34",
+        "file": "09-小小探索家 測謊機為什麼知道我說謊？.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "臺灣好好玩 來雲林口湖撈海鮮",
+        "track": 10,
+        "duration": "4:13",
+        "file": "10-臺灣好好玩 來雲林口湖撈海鮮.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "故事摩天輪 小山精和夏天的作業1",
+        "track": 11,
+        "duration": "5:35",
+        "file": "11-故事摩天輪 小山精和夏天的作業1.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "小行星點讀ABC The Bear Went Over The Mountain",
+        "track": 12,
+        "duration": "10:22",
+        "file": "12-小行星點讀ABC The Bear Went Over The Mountain.mp3"
+    },
+    {
+        "album": "63期",
+        "title": "2021年6月號小行星的祝福",
+        "track": 13,
+        "duration": "3:02",
+        "file": "13-2021年6月號小行星的祝福.mp3"
+    }
+];
---
+++ medias/小行星64期-跟著地圖去旅行/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "64期",
+        "title": "2021年7月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01-2021年7月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "情緒劇場 樂樂迷路了",
+        "track": "02",
+        "duration": "4:51",
+        "file": "02-情緒劇場 樂樂迷路了.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "幼年童話 糟糕，地圖游走了？",
+        "track": "03",
+        "duration": "4:44",
+        "file": "03-幼年童話 糟糕，地圖游走了？.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "動物新鮮事 小鴿子要回家",
+        "track": "04",
+        "duration": "4:07",
+        "file": "04-動物新鮮事 小鴿子要回家.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "知識不思議 符號捉迷藏",
+        "track": "05",
+        "duration": "10:30",
+        "file": "05-知識不思議 符號捉迷藏.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "讀首小詩 夏天去哪玩？",
+        "track": "06",
+        "duration": "2:29",
+        "file": "06-讀首小詩 夏天去哪玩？.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "腦筋轉轉彎 我想待在家裡",
+        "track": "07",
+        "duration": "3:44",
+        "file": "07-腦筋轉轉彎 我想待在家裡.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "小小探索家 紅綠燈是怎麼設計的？",
+        "track": "08",
+        "duration": "5:33",
+        "file": "08-小小探索家 紅綠燈是怎麼設計的？.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "身體怎麼了 搔搔癢Tickle Tickle",
+        "track": "09",
+        "duration": "2:51",
+        "file": "09-身體怎麼了 搔搔癢Tickle Tickle.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "故事音樂盒 長髮公主",
+        "track": 10,
+        "duration": "9:35",
+        "file": "10-故事音樂盒 長髮公主.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "故事摩天輪 小山精和夏天的作業2",
+        "track": 11,
+        "duration": "5:16",
+        "file": "11-故事摩天輪 小山精和夏天的作業2.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "小行星點讀ABC The Muffin Man",
+        "track": 12,
+        "duration": "12:43",
+        "file": "12-小行星點讀ABC The Muffin Man.mp3"
+    },
+    {
+        "album": "64期",
+        "title": "2021年7月號小行星的祝福",
+        "track": 13,
+        "duration": "3:02",
+        "file": "13-2021年7月號小行星的祝福.mp3"
+    }
+];
