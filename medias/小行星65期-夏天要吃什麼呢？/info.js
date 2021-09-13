21f4a663e387568aecf90a2506f6a3c272d545e4
AngusKo <anan20.tw@gmail.com>
2021-09-13 11:01:20 +0800

    65.66 期上架

---
+++ medias/小行星65期-夏天要吃什麼呢？/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "65期",
+        "title": "2021年8月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01-2021年8月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "情緒劇場 西瓜壞掉了",
+        "track": "02",
+        "duration": "6:55",
+        "file": "02-情緒劇場 西瓜壞掉了.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "讀首小詩 好多花兒",
+        "track": "03",
+        "duration": "2:11",
+        "file": "03-讀首小詩 好多花兒.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "動物新鮮事 守瓜守著瓜",
+        "track": "04",
+        "duration": "3:38",
+        "file": "04-動物新鮮事 守瓜守著瓜.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "知識不思議 夏天吃什麼？",
+        "track": "05",
+        "duration": "8:04",
+        "file": "05- 知識不思議 夏天吃什麼？.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "幼年童話 瓜田裡的新娃娃",
+        "track": "06",
+        "duration": "3:56",
+        "file": "06-幼年童話 瓜田裡的新娃娃.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "腦筋轉轉彎 我一定要擦防晒乳嗎？",
+        "track": "07",
+        "duration": "3:01",
+        "file": "07-腦筋轉轉彎 我一定要擦防晒乳嗎？.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "小小探索家 罐頭為什麼能保鮮？",
+        "track": "08",
+        "duration": "4:06",
+        "file": "08-小小探索家 罐頭為什麼能保鮮？.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "身體怎麼了 媽媽的脣疱疹",
+        "track": "09",
+        "duration": "3:09",
+        "file": "09-身體怎麼了 媽媽的脣疱疹.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "STEAM這樣玩 好玩的潛水艇",
+        "track": 10,
+        "duration": "5:12",
+        "file": "10-STEAM這樣玩 好玩的潛水艇.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "故事摩天輪 小山精和夏天的作業3",
+        "track": 11,
+        "duration": "5:26",
+        "file": "11-故事摩天輪 小山精和夏天的作業3.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "小行星點讀ABC Five Little Monkeys",
+        "track": 12,
+        "duration": "13:28",
+        "file": "12-小行星點讀ABC Five Little Monkeys.mp3"
+    },
+    {
+        "album": "65期",
+        "title": "2021年8月號小行星的祝福",
+        "track": 13,
+        "duration": "2:55",
+        "file": "13-2021年8月號小行星的祝福.mp3"
+    }
+];
---
+++ medias/小行星66期-我的開學日/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "66期",
+        "title": "2021年9月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01-2021年9月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "情緒劇場 奇奇學游泳",
+        "track": "02",
+        "duration": "7:29",
+        "file": "02-情緒劇場 奇奇學游泳.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "讀首小詩 蒲公英與氣球",
+        "track": "03",
+        "duration": "2:49",
+        "file": "03-讀首小詩 蒲公英與氣球.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "腦筋轉轉彎 弄壞別人的東西怎麼辦？",
+        "track": "04",
+        "duration": "3:53",
+        "file": "04-腦筋轉轉彎 弄壞別人的東西怎麼辦？.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "知識不思議 我的開學日",
+        "track": "05",
+        "duration": "9:52",
+        "file": "05-知識不思議 我的開學日.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "幼年童話 小花的煩惱",
+        "track": "06",
+        "duration": "5:07",
+        "file": "06-幼年童話 小花的煩惱.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "動物新鮮事 誰當獅子王？",
+        "track": "07",
+        "duration": "4:22",
+        "file": "07-動物新鮮事 誰當獅子王？.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "身體怎麼了 我的牙朋友",
+        "track": "08",
+        "duration": "4:07",
+        "file": "08-身體怎麼了 我的牙朋友.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "小小探索家 為什麼保溫瓶會保溫？",
+        "track": "09",
+        "duration": "5:05",
+        "file": "09-小小探索家 為什麼保溫瓶會保溫？.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "故事音樂盒 美女與野獸",
+        "track": 10,
+        "duration": "9:25",
+        "file": "10-故事音樂盒 美女與野獸.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "故事摩天輪 心想事成魔法店1",
+        "track": 11,
+        "duration": "5:43",
+        "file": "11-故事摩天輪 心想事成魔法店1.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "小行星點讀ABC Two Little Black Birds",
+        "track": 12,
+        "duration": "11:05",
+        "file": "12-小行星點讀ABC Two Little Black Birds.mp3"
+    },
+    {
+        "album": "66期",
+        "title": "2021年9月號小行星的祝福",
+        "track": 13,
+        "duration": "3:02",
+        "file": "13-2021年9月號小行星的祝福.mp3"
+    }
+];
