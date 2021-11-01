e12b63c0de9fcb6e8f50441b6b28376d199513e1
AngusKo <anan20.tw@gmail.com>
2021-11-01 12:01:14 +0800

    67.68 期上架

---
+++ medias/小行星67期-有話好好說/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "67期",
+        "title": "2021年10月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01 2021年10月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "情緒劇場 樂樂要去哪裡？",
+        "track": "02",
+        "duration": "7:47",
+        "file": "02 情緒劇場 樂樂要去哪裡？.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "讀首小詩 有話好好說",
+        "track": "03",
+        "duration": "2:37",
+        "file": "03 讀首小詩 有話好好說.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "動物新鮮事 灰狼來集合",
+        "track": "04",
+        "duration": "3:37",
+        "file": "04 動物新鮮事 灰狼來集合.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "知識不思議 溝通真有趣",
+        "track": "05",
+        "duration": "8:51",
+        "file": "05 知識不思議 溝通真有趣.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "腦筋轉轉彎 被別人罵了，怎麼辦？",
+        "track": "06",
+        "duration": "3:17",
+        "file": "06 腦筋轉轉彎 被別人罵了，怎麼辦？.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "小小探索家 植物也會聊天說話嗎？",
+        "track": "07",
+        "duration": "5:27",
+        "file": "07 小小探索家 植物也會聊天說話嗎？.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "幼年童話 想學跳舞的小兔子",
+        "track": "08",
+        "duration": "5:17",
+        "file": "08 幼年童話 想學跳舞的小兔子.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "身體怎麼了 疱疹病毒黑皮斯",
+        "track": "09",
+        "duration": "4:18",
+        "file": "09 身體怎麼了 疱疹病毒黑皮斯.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "世界大不同 探索寶藏之國南非",
+        "track": 10,
+        "duration": "5:46",
+        "file": "10 世界大不同 探索寶藏之國南非.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "故事摩天輪 心想事成魔法店2",
+        "track": 11,
+        "duration": "6:41",
+        "file": "11 故事摩天輪 心想事成魔法店2.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "小行星點讀ABC One POTAT, Two POTATO",
+        "track": 12,
+        "duration": "11:05",
+        "file": "12 小行星點讀ABC One POTAT, Two POTATO.mp3"
+    },
+    {
+        "album": "67期",
+        "title": "2021年10月號小行星的祝福",
+        "track": 13,
+        "duration": "3:02",
+        "file": "13 2021年10月號小行星的祝福.mp3"
+    }
+];
---
+++ medias/小行星68期-來逛商店街/info.js
@@ -0,0 +1,96 @@
+// 1. 將輸出的 csv 轉成 json，https://csvjson.com/csv2json
+// 2. 將 [] 替換掉
+var fileInfo =
+[
+    {
+        "album": "68期",
+        "title": "2021年11月號小行星幼兒誌",
+        "track": "01",
+        "duration": "3:05",
+        "file": "01 2021年11月號小行星幼兒誌.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "情緒劇場 康康的零用錢",
+        "track": "02",
+        "duration": "7:09",
+        "file": "02 情緒劇場 康康的零用錢.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "讀首小詩 禮物",
+        "track": "03",
+        "duration": "2:55",
+        "file": "03 讀首小詩 禮物.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "腦筋轉轉彎 我一定要存錢嗎？",
+        "track": "04",
+        "duration": "4:20",
+        "file": "04 腦筋轉轉彎 我一定要存錢嗎？.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "知識不思議 來逛商店街",
+        "track": "05",
+        "duration": "9:41",
+        "file": "05 知識不思議 來逛商店街.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "動物新鮮事 小錢鼠來逛街？",
+        "track": "06",
+        "duration": "3:59",
+        "file": "06 動物新鮮事 小錢鼠來逛街？.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "小小探索家 紙鈔破掉了怎麼辦？",
+        "track": "07",
+        "duration": "4:23",
+        "file": "07 小小探索家 紙鈔破掉了怎麼辦？.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "幼年童話 賣光嘍！",
+        "track": "08",
+        "duration": "5:02",
+        "file": "08 幼年童話 賣光嘍！.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "身體怎麼了 我會小心新冠病毒",
+        "track": "09",
+        "duration": "2:57",
+        "file": "09 身體怎麼了 我會小心新冠病毒.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "臺灣好好玩 到臺南官田採菱角",
+        "track": 10,
+        "duration": "5:24",
+        "file": "10 臺灣好好玩 到臺南官田採菱角.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "故事摩天輪 心想事成魔法店3",
+        "track": 11,
+        "duration": "5:54",
+        "file": "11 故事摩天輪 心想事成魔法店3.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "小行星點讀ABC This Old Man",
+        "track": 12,
+        "duration": "11:20",
+        "file": "12 小行星點讀ABC This Old Man.mp3"
+    },
+    {
+        "album": "68期",
+        "title": "2021年11月號小行星的祝福",
+        "track": 13,
+        "duration": "3:02",
+        "file": "13 2021年11月號小行星的祝福.mp3"
+    }
+];
